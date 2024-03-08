6dba75f517721061c1225f5ae8dd96f38de7b761
ayan <mohdayanrehman07@gmail.com>
2024-03-08 11:37:38 +0530

    first commit

---
+++ Dsa/dsa.js
@@ -0,0 +1,25 @@
+//Array-Traversing -> meaning print every array in solo
+
+// let data = [9, 49, 2, 6, 7, 8, 5, 4, 3, 12, 15];
+// for (let i = 0; i < data.length; i++) {
+//   {
+//     document.write(`Array ${i} ${data[i]} <br>`);
+
+//   }
+// }
+
+//only for third number data
+// let x = 7;
+// document.write(data[x]);
+
+//aray ke number print karana
+// function getElement() {
+//   let el = document.getElementById("element").value;
+//   {
+//     if (el < data.length && parseInt(el) === "number" && el >= 0) {
+//       alert( data[el]);
+//     } else {
+//       alert("please Enter valid value!!!!!!!");
+//     }
+//   }
+// }
